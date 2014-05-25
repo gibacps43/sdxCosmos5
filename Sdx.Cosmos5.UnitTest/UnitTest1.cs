1bdd36511e155a58181f1a17acf01739daaf3e5f
gibacps43 <gibacps43@gmail.com>
2014-05-25 11:07:13 -0300

    Versão Inicial

---
+++ Sdx.Cosmos5.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sdx.Cosmos5.UnitTest
+{
+    [TestClass]
+    public class UnitTestSdx
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Sdx.Cosmos5.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sdx.Cosmos5.Web.MvcApplication" Language="C#" %>
